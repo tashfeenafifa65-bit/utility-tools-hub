991d29ef86c771d560f818dfa1cc2fa1447e60b3
tashfeenafifa65-bit <tashfeenafifa65@gmail.com>
2026-06-29 05:46:55 +0500

    Added PDF to Image and AI Background Remover tools

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from PIL import Image
 import io
+from pdf2image import convert_from_path
+from zipfile import ZipFile
+import tempfile
+import shutil
+from rembg import remove
+from PIL import Image
+import io
+import os
+import tempfile
+from flask import request, send_file
+
 
 
 
@@ -26,6 +37,8 @@ with app.app_context():
 @app.route('/')
 def index():
     tools = [
+        {'id': 'background-remover','name': 'AI Background Remover','description': 'Remove image backgrounds automatically using AI. Supports JPG, PNG and WEBP.','icon': 'fa-solid fa-scissors'},
+        {'id': 'pdf-to-image','name': 'PDF to Image Converter','description': 'Convert PDF pages into JPG, PNG, WEBP and TIFF images online.','icon': 'fa-file-image'},
         {'id': 'image-resizer','name': 'Image Resizer','description': 'Resize JPG, PNG, and WEBP images online instantly.','icon': 'fa-expand'},
         {'id': 'image-compressor','name': 'Image Compressor','description': 'Compress JPG, PNG and WEBP images online without losing quality.','icon': 'fa-compress'},
         {'id': 'percentage-calculator', 'name': 'Percentage Calculator', 'description': 'Calculate percentages, increases, decreases and percentage differences instantly.', 'icon': 'fa-percent'},
@@ -91,6 +104,96 @@ def sitemap():
 @app.route('/tool/qr-code-generator')
 def qr_code_generator():
     return render_template('tools/qr-code-generator.html')
+
+@app.route('/convert-pdf-to-image', methods=['POST'])
+def convert_pdf_to_image():
+
+    pdf_file = request.files['pdf']
+    output_format = request.form['format']
+
+    temp_dir = tempfile.mkdtemp()
+
+    pdf_path = os.path.join(temp_dir, pdf_file.filename)
+    pdf_file.save(pdf_path)
+
+    pages = convert_from_path(pdf_path)
+
+    zip_path = os.path.join(temp_dir, "converted_images.zip")
+
+    format_map = {
+        'jpg': 'JPEG',
+        'png': 'PNG',
+        'webp': 'WEBP',
+        'tiff': 'TIFF'
+    }
+
+    with ZipFile(zip_path, 'w') as zipf:
+
+        for i, page in enumerate(pages):
+
+            image_name = f"page_{i+1}.{output_format}"
+
+            image_path = os.path.join(
+                temp_dir,
+                image_name
+            )
+
+            # JPEG does not support RGBA
+            if output_format.lower() == 'jpg':
+                page = page.convert("RGB")
+
+            page.save(
+                image_path,
+                format_map[output_format.lower()]
+            )
+
+            zipf.write(
+                image_path,
+                image_name
+            )
+
+    return send_file(
+        zip_path,
+        as_attachment=True,
+        download_name="pdf-images.zip"
+    )
+
+from flask import jsonify
+import base64
+
+@app.route('/remove-background', methods=['POST'])
+def remove_background():
+
+
+    if 'image' not in request.files:
+        return jsonify({"success":False})
+
+    file=request.files['image']
+
+    try:
+
+        input_bytes=file.read()
+
+        output_bytes=remove(input_bytes)
+
+        image_base64=base64.b64encode(output_bytes).decode("utf-8")
+
+        return jsonify({
+            "success":True,
+            "image":"data:image/png;base64,"+image_base64
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "success":False,
+            "error":str(e)
+        })
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
